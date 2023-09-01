639388f0af6301677f5b63dc152ba337375a0290
banoczymartin <banoczy.martin@stud.uni-obuda.hu>
2023-09-01 23:05:34 +0200

    Specific_shoe_details table created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Shoe> Shoes { get; set; }
+    public DbSet<SpecificShoe> Specific_shoe_details { get; set; }
+
 
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
