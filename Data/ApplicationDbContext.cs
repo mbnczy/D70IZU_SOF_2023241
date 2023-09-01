58dca7d1a738b38e0bdaaad20ae33fdb948b6103
banoczymartin <banoczy.martin@stud.uni-obuda.hu>
2023-09-01 18:29:03 +0200

    color dbset created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ShoeWebshop.Models;
 
 namespace ShoeWebshop.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Color> Colors { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
