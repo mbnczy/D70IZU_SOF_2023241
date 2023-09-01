0880d3b6994bf48ed4774305041c398b2a65d7f3
banoczymartin <banoczy.martin@stud.uni-obuda.hu>
2023-09-01 23:10:36 +0200

    purchase table created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Shoe> Shoes { get; set; }
     public DbSet<SpecificShoe> Specific_shoe_details { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
 
 
 
---
+++ Models/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoeWebshop.Models
+{
+	public class Purchase
+	{
+		[Key]
+		public string PurchaseID { get; set; }
+		public string CustomerID { get; set; }
+		public DateTime Purchase_date { get; set; }
+		public string Total_amount { get; set; }
+
+		public Purchase()
+		{
+			PurchaseID = Guid.NewGuid().ToString();
+		}
+	}
+}
+
