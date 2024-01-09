48467371f1405a63653d5d50ce3df70795b5075f
Kristof0101 <vargika0@gmail.com>
2024-01-09 10:57:11 +0100

    api controller class

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoeWebshop.Models;
+
+namespace ShoeWebshop.Controllers
+{
+
+    [ApiController]
+
+    [Route("[controller]")]
+    public class ApiController:ControllerBase
+    {
+        UserManager<SiteUser> _userManager;
+
+        public ApiController(UserManager<SiteUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<SiteUser> GetUsers()
+        {
+            return this._userManager.Users;
+        }
+    }
+}
